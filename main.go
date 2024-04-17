c72b7f05e1525026ef7612843539d24ea8ecfe93
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-04-17 18:20:51 +0900

    最低限ハンドラを実装(ダミー)

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Server listening on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
