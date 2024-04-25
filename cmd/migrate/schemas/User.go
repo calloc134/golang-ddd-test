9ccb5f3ab473e717c9457d638e15aea629d92ef5
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-04-25 10:23:05 +0900

    versionカラムの追加

--- cmd/migrate/schemas/User.go
+++ cmd/migrate/schemas/User.go
@@ -3,9 +3,10 @@ package schemas
 import "github.com/uptrace/bun"
 
 type UserTable struct {
-	bun.BaseModel `bun:"table:users,alias:users"`
-	ULID 		string `bun:"ulid,pk,type:vargchar(26)"`
+	bun.BaseModel `bun:"table:users"`
+	ULID 		string `bun:"pk,type:vargchar(26)"`
 	Name 		string `bun:"type:varchar(255)"`
-	Age 		int
+	Age 		int 
+	Version 	int `bun:"version"`
 }
 
