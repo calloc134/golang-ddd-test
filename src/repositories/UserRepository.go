0c77bc8ae5f051a5ecd4101655aa119f7c6ea109
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-04-28 19:03:10 +0900

    永続化部分の処理修正

--- src/repositories/UserRepository.go
+++ src/repositories/UserRepository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/calloc134/golang-ddd-test/cmd/migrate/schemas"
 	"github.com/calloc134/golang-ddd-test/src/application"
@@ -18,52 +19,54 @@ func NewUserRepository(db *bun.DB) application.IUserRepository {
 	return UserRepository{db: db}
 }
 
-func (ur UserRepository) FindAll(context context.Context) ([]domain.UserAggregate, error) {
+func (ur UserRepository) FindAll(context context.Context) ([]domain.User, error) {
 
 	userTables := []schemas.UserTable{}
 
-
-	err := ur.db.NewSelect().Model((&userTables)).Scan(context)
+	err := ur.db.NewSelect().Model((&userTables)).Relation("UserDetail").Scan(context)
 
 	if err != nil {
 		return nil, err
 	}
 
-	var users []domain.UserAggregate
-	
+	var users []domain.User
+
 	for _, userTable := range userTables {
-		users = append(users, domain.UserAggregate{
-			ULID: userTable.ULID,
-			Name: userTable.Name,
-			Age: userTable.Age,
+		users = append(users, domain.User{
+			ULID:    userTable.ULID,
 			Version: userTable.Version,
+			UserDetail: &domain.UserDetail{
+				Name: userTable.UserDetail.Name,
+				Age:  userTable.UserDetail.Age,
+			},
 		})
 	}
 
 	return users, nil
 }
 
-func (ur UserRepository) FindByUlid(context context.Context, uuid string) (*domain.UserAggregate, error) {
-	
+func (ur UserRepository) FindByUlid(context context.Context, uuid string) (*domain.User, error) {
+
 	userTable := schemas.UserTable{}
 
-	err := ur.db.NewSelect().Model(&userTable).Where("ulid = ?", uuid).Scan(context)
+	err := ur.db.NewSelect().Model(&userTable).Where("ulid = ?", uuid).Relation("UserDetail").Scan(context)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &domain.UserAggregate{
+	return &domain.User{
 		ULID: userTable.ULID,
-		Name: userTable.Name,
-		Age: userTable.Age,
+		UserDetail: &domain.UserDetail{
+			Name: userTable.UserDetail.Name,
+			Age:  userTable.UserDetail.Age,
+		},
 		Version: userTable.Version,
 	}, nil
-	
-	
+
 }
 
-func (ur UserRepository) Save(context context.Context, user *domain.UserAggregate) (*domain.UserAggregate, error) {
+func (ur UserRepository) Save(context context.Context, user *domain.User) (*domain.User, error) {
 
 	tx, err := ur.db.BeginTx(context, nil)
 
@@ -75,36 +78,45 @@ func (ur UserRepository) Save(context context.Context, user *domain.UserAggregat
 
 	// 楽観的ロックの判定
 	// ulidが一致したらversionを取得 versionが一致したらパス
-	var version int;
-	err = tx.NewSelect().Model(&schemas.UserTable{}).Column("version").Where("ulid = ?", user.ULID).Scan(context, &version);
-	
-	// データがない場合はスルー
+	var version int
+	versionCheckErr := tx.NewSelect().Model(&schemas.UserTable{}).Column("version").Where("ulid = ?", user.ULID).Scan(context, &version)
+
 	// データがあってversionが一致しない場合はエラー
-	if err != nil && version != user.Version {
+	if versionCheckErr != nil && version != user.Version {
 		return nil, errors.New("楽観的ロックエラー")
 	}
 
 	userTable := schemas.UserTable{
-		ULID: user.ULID,
-		Name: user.Name,
-		Age: user.Age,
+		ULID:    user.ULID,
 		Version: user.Version + 1,
 	}
 
-	// データがある場合は削除
-	// 更新時に過去データを削除するか、削除はバッチ処理にして参照の条件を工夫するかは要検討
-	if err == nil || err.Error() != "bun: no rows in result set" {
-		_, err = tx.NewDelete().Model(&userTable).Where("ulid = ?", user.ULID).Exec(context);
-		
-		if err != nil {
-			return nil, err
-		}
+	userDetailTable := schemas.UserDetailTable{
+		Name:   user.UserDetail.Name,
+		Age:    user.UserDetail.Age,
+		UserID: user.ULID,
 	}
-	
-	_, err = tx.NewInsert().Model(&userTable).Exec(context);
+
+	_, err = tx.NewInsert().Model(&userDetailTable).Exec(context)
 
 	if err != nil {
 		return nil, err
+	}
+
+	if versionCheckErr != nil && versionCheckErr.Error() == "sql: no rows in result set" {
+		fmt.Println("insert")
+		_, err = tx.NewInsert().Model(&userTable).Exec(context)
+
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		fmt.Println("update")
+		_, err = tx.NewUpdate().Model(&userTable).Set("version = ?", userTable.Version).Where("ulid = ?", user.ULID).Exec(context)
+
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	err = tx.Commit()
@@ -113,12 +125,5 @@ func (ur UserRepository) Save(context context.Context, user *domain.UserAggregat
 		return nil, err
 	}
 
-	return &domain.UserAggregate{
-		ULID: userTable.ULID,
-		Name: userTable.Name,
-		Age: userTable.Age,
-		Version: userTable.Version,
-	}, nil
+	return user, nil
 }
-
-
