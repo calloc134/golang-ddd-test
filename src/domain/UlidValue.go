4b2f48bf5594cbc6cbc34b916ad6e459b1cdc1f8
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-05-02 11:45:39 +0900

    ULIDの値オブジェクトを実装

---
+++ src/domain/UlidValue.go
@@ -0,0 +1,52 @@
+package domain
+
+import (
+	"errors"
+	"math/rand"
+	"time"
+
+	"github.com/oklog/ulid/v2"
+)
+
+type UlidValue struct {
+	value string
+}
+
+func GenerateULID() (*UlidValue, error) {
+	entropy := rand.New(rand.NewSource(time.Now().UnixNano()))
+	ms := ulid.Timestamp(time.Now())
+	ulid, err := ulid.New(ms, entropy)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ulidValue, err := NewULID(ulid.String())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ulidValue, nil
+}
+
+func NewULID(value string) (*UlidValue, error) {
+	if value == "" {
+		return nil, ErrEmptyULID
+	}
+
+	if len(value) != 26 {
+		return nil, ErrInvalidULID
+	}
+
+	return &UlidValue{value: value}, nil
+}
+
+func (u *UlidValue) Equals(target *UlidValue) bool {
+	return u.value == target.value
+}
+
+var (
+	ErrEmptyULID   = errors.New("empty ulid")
+	ErrInvalidULID = errors.New("invalid ulid")
+)
